e804ac5ee2316cdd008c5959979bd852b8420ce8
Nippun Kumaar <nippun05@gmail.com>
2022-03-28 11:12:24 +0530

    Added loops examples

---
+++ loops/Ex4.c
@@ -0,0 +1,15 @@
+//Example : do...while loop
+// Program to add numbers until user enters zero
+#include <stdio.h>
+int main()
+{    double number, sum = 0;
+    // loop body is executed at least once
+    do
+    {
+        printf("Enter a number: ");
+        scanf("%lf", &number);
+        sum += number;
+    }
+    while(number != 0.0);
+    printf("Sum = %.2lf\n",sum);
+}
